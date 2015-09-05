17c6c26e1f5e64fe9d23038fa90172ba33f2062a
Sergey Romanov <xxsmotur@gmail.com>
2015-09-06 02:58:41 +0500

    first commit

---
+++ bot.go
@@ -0,0 +1,71 @@
+package telgitbot
+
+import (
+	"fmt"
+	"github.com/Syfaro/telegram-bot-api"
+	"github.com/google/go-github/github"
+	"log"
+	"strings"
+	"time"
+)
+
+type Telgitbot struct {
+	botapi *tgbotapi.BotAPI
+	client *github.Client
+}
+
+func New(token string) *Telgitbot {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	tgb := new(Telgitbot)
+	tgb.botapi = bot
+	tgb.client = github.NewClient(nil)
+	return tgb
+}
+
+func (tgb *Telgitbot) Start() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	err := tgb.botapi.UpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for {
+		for update := range tgb.botapi.Updates {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+			text := update.Message.Text
+			if strings.HasPrefix(text, "/repos_") {
+				username := strings.Split(text, "_")[1]
+				if len(username) > 0 {
+					//opt := &github.RepositoryListOptions{Sort: "updated"}
+					repos, _, err := tgb.client.Repositories.List(username, nil)
+					if err != nil {
+						fmt.Println("error: %v\n\n", err)
+					} else {
+						result := ""
+						for _, repo := range repos {
+							result += *repo.FullName + "\n"
+						}
+						msg := tgbotapi.NewMessage(update.Message.Chat.ID, result)
+						tgb.botapi.SendMessage(msg)
+					}
+				}
+			}
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func (tgb *Telgitbot) findByStars(title string) {
+
+}
